b5a5d1588fdd35f58b6a5c6b928748d2eb3ec164
Michael Huang <mhuang19@cs.washington.edu>
2020-05-17 14:55:24 -0700

    B-ST1 marking should be exactly right!

--- plotting/plotanalysis.py
+++ plotting/plotanalysis.py
@@ -4,7 +4,7 @@ from PIL import Image
 import csv
 
 dir = './figures/'
-paperPNG = 'coloredA-ST1paper.png'
+paperPNG = 'coloredB-ST1paper.png'
 
 im = Image.open(dir + paperPNG)
 pic = im.load()
@@ -21,19 +21,22 @@ axo = 4.6
 ayo = 10.8
 axi = 29
 ayi = 30
+aPreset = {"x" : ax, "y" : ay, "xo" : axo, "yo" : ayo, "xi" : axi, "yi" : ayi}
 
 # pre-sets for B-ST1
-# x starts around 5.8, but is really 15 at left
-# y starts around 0, but is really 21 at top
+# x starts around 6.0 but is really 15 at left
+# y starts around 3, but is really 21 at top
 # 784 x 869, 224 values
 # non-inclusive
 bx = range(15, 33)
 by = range(2, 22)
-bxo = 5.8
-byo = 0
-bxi = 40
-byi = 40
+bxo = 6
+byo = 3
+bxi = 44
+byi = 44
+bPreset = {"x" : bx, "y" : by, "xo" : bxo, "yo" : byo, "xi" : bxi, "yi" : byi}
 
+# color tuples and dicts
 pink = (229, 42, 137, 255)
 blue = (118, 112, 178, 255)
 grey = (102, 102, 102, 255)
@@ -43,25 +46,23 @@ turq = (141, 208, 197, 255)
 aDict = {pink:"pink", blue:"blue", grey:"grey", green:"green"}
 bDict = {pink:"pink", turq:"turq", grey:"grey"}
 
-colorDict = aDict
+# TODO: EDIT THIS VALUE HERE WHEN RE-RUNNING
+preset = bPreset
+colorDict = bDict
 count = 0
-xValues = ax
-yValues = ay
-xOffset = axo
-yOffset = ayo
-xIter = axi
-yIter = ayi
 
+# Actual reader
 with open(paperPNG + '.csv', mode='w', newline='') as file:
 
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    for x in xValues:
-        for y in yValues:
-            xcoord = xOffset + ((x - min(xValues)) * axi)
-            ycoord = yOffset + ((y - min(yValues)) * ayi)
+    for x in preset["x"]:
+        for y in preset["y"]:
+            xcoord = preset["xo"] + ((x - min(preset["x"])) * preset["xi"])
+            ycoord = preset["yo"] + ((y - min(preset["y"])) * preset["yi"])
 
             color = pic[xcoord, ycoord]
+            print(str(xcoord) + ", " + str(ycoord))
             if color != (255,255,255,255):
                 print(str(xcoord) + ", " + str(ycoord))
                 print(str(x) + ", " + str(y))
@@ -73,5 +74,4 @@ with open(paperPNG + '.csv', mode='w', newline='') as file:
 print(count)
 
 
-# TODO: annotate in csv
 # TODO: work on ggplot, which might be easier and more accurate
