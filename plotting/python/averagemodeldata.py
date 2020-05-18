47cb4bedc6d12ef6ceca4e7a1f1ba42928175c60
Michael Huang <mhuang19@cs.washington.edu>
2020-05-18 03:13:02 -0700

    A-ST1 averages outputted properly

--- plotting/python/averagemodeldata.py
+++ plotting/python/averagemodeldata.py
@@ -2,20 +2,22 @@ import csv
 import numpy as np
 import pandas as pd
 
-# version = 'GSM3036911_PDAC-A-ST1'
-version = 'GSM3405534_PDAC-B-ST1'
+version = 'GSM3036911_PDAC-A-ST1'
+# version = 'GSM3405534_PDAC-B-ST1'
 colorFile = '../data/' + version + 'colors.csv'
 dataFile = '../data/' + version + '.tsv'
 
 colordf = pd.read_csv(colorFile)
 data = pd.read_csv(dataFile, sep='\t')
 
+# data = data.rename(index = data.iloc[:,0])
 # We want to ensure that all locations are at the top, as column headers.
 if (version == 'GSM3036911_PDAC-A-ST1'):
     data = data.T
 
+data = data.rename(columns = data.iloc[0])
+data = data.drop(data.index[0])
 # print(data)
-# print(data['Genes'])
 
 colorDict = dict()
 
@@ -29,21 +31,20 @@ for i in range(0, len(colordf)):
 
     colorDict[color].add(coord)
 
-# print(colorDict)
-# data[]
-
 for category in colorDict:
     with open(version + category + '.csv', mode='w', newline='') as file:
         writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        df = pd.DataFrame(data['Genes'])
+        # df = pd.DataFrame(data['Genes'])
+        df = pd.DataFrame(data.index)
+        df = df.set_index(data.index)
+
+        # writer.writerow(data['Genes'])
+        writer.writerow(df.iloc[:,0])
+        df = df.iloc[:,1:]
 
         for area in colorDict[category]:
             df[str(area)] = data[str(area)]
 
-        writer.writerow(data['Genes'])
         writer.writerow(df.mean(axis=1))
-        # writer.writerow(data[str(area)])
 
-            # print(colorDict[category])
-
-        print(df)
+        # print(df)
