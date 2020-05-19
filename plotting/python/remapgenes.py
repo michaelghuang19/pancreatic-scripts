948241ea0619c5aa404376a013cfc02859b6cb4e
Michael Huang <mhuang19@cs.washington.edu>
2020-05-19 03:55:55 -0700

    reformatted entrezgen key to be parsable

--- plotting/python/remapgenes.py
+++ plotting/python/remapgenes.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-dataindex = '../data'
-genekeyfile = dataindex + 'Human_gene_ID_mapping_file.csv'
+dataindex = '../data/'
+genekeyfile = dataindex + 'entrezgen-hgnc-table.csv'
 datafile = dataindex + 'average-B-ST1grey.csv'
 
 genekey = pd.read_csv(genekeyfile)
 data = pd.read_csv(datafile)
 
-# TODO: change
+genekey = genekey.dropna()
+
+print(genekey)
+
+
 
 # evaluate only metabolic, median?
