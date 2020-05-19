ee3ab5d7a6dc75c1fc68294af7eea24ac744769b
Michael Huang <mhuang19@cs.washington.edu>
2020-05-19 15:30:44 -0700

    reorganized data, working on getting top 500 expression levels

--- plotting/python/remapgenes.py
+++ plotting/python/remapgenes.py
@@ -1,37 +1,77 @@
 import pandas as pd
+import numpy as np
+import csv
 
 dataindex = '../data/'
-genekeyfile = dataindex + 'entrezgene-hgnc-table.csv'
-datafile = dataindex + 'average-A-ST1rey.csv'
+# version = 'B-ST1blue'
+versionSet = {'B-ST1grey', 'B-ST1pink', 'B-ST1turq', 'A-ST1blue', 'A-ST1green', 'A-ST1pink', 'A-ST1rey'}
 
-genekey = pd.read_csv(genekeyfile)
-data = pd.read_csv(datafile, header = None)
+def convert_hgnc_to_entrez():
+    genekeyfile = dataindex + 'entrez/entrezgene-hgnc-table.csv'
+    datafile = dataindex + 'average-' + version + '.csv'
 
-genekey = genekey.dropna()
-genekey = genekey.drop_duplicates()
-genekey['entrezgene'] = genekey['entrezgene'].astype(int)
-data.columns = {'genes', 'state'}
+    genekey = pd.read_csv(genekeyfile)
+    data = pd.read_csv(datafile, header = None)
 
-# print(genekey)
-# print(data)
-# print(len(data))
+    genekey = genekey.dropna()
+    genekey = genekey.drop_duplicates()
+    genekey['entrezgene'] = genekey['entrezgene'].astype(int)
+    data.columns = {'genes', 'state'}
 
-for i in range(0,len(data)):
-    hgnc_gene = data.iloc[i]['genes']
-    entrez_geneseries = genekey.loc[genekey['hgnc_symbol'] == hgnc_gene]
+    # print(genekey)
+    # print(data)
+    # print(len(data))
 
-    print(hgnc_gene)
+    for i in range(0,len(data)):
+        hgnc_gene = data.iloc[i]['genes']
+        entrez_geneseries = genekey.loc[genekey['hgnc_symbol'] == hgnc_gene]
 
-    if entrez_geneseries.empty:
-        print(entrez_geneseries)
-        data.drop([i])
-    else:
-        index = entrez_geneseries.index[0]
-        print(index)
-        data.at[i, 'genes'] = genekey.at[index, 'entrezgene']
+        print(hgnc_gene)
 
-data.to_csv('entrez-A-ST1rey.csv', index = False)
+        if entrez_geneseries.empty:
+            print(entrez_geneseries)
+            data.drop([i])
+        else:
+            index = entrez_geneseries.index[0]
+            print(index)
+            data.at[i, 'genes'] = genekey.at[index, 'entrezgene']
+
+    data.to_csv('entrez-A-ST1rey.csv', index = False)
+
+def get_metabolic():
+    metabolicfile = dataindex + 'section_1point2_tumor_region_evidence_mean.txt'
+
+    meta_data = pd.read_csv(metabolicfile, sep = '\t')
+    meta_set = set(meta_data['genes'])
+
+    return meta_set
 
 
+# evaluate only metabolic?
+def keep_metabolic(meta_set, version):
+    datafile = dataindex + 'entrez/entrez-' + version + '.csv'
+    data = pd.read_csv(datafile)
 
-# evaluate only metabolic, median?
+    with open('metabolic' + version + '.csv', mode='w', newline='') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        for i in range(0, len(data)):
+            series = data.iloc[i]
+            entrez = series['genes']
+
+            if entrez.isnumeric():
+                entrez = int(entrez)
+
+                if entrez in meta_set:
+                    writer.writerow(series)
+
+def get_median(version):
+    datafile = dataindex + 'entrez/entrez-' + version + '.csv'
+    metafile = dataindex + 'metabolic/metabolic-' + version + '.csv'
+
+# only keep median
+
+if __name__ == "__main__":
+    meta_set = get_metabolic()
+    for version in versionSet:
+        keep_metabolic(meta_set, version)
