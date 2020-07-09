ee6316dd6d0438b61a69d14430bce3abe979a74c
Michael Huang <mhuang19@cs.washington.edu>
2020-07-09 01:07:51 -0700

    added biomass components and alternative gene data

--- matlab/dls_batch.m
+++ matlab/dls_batch.m
@@ -15,20 +15,23 @@ diff_lethal_cancer = cancer.genes(grRatio_cancer<0.1 & grRatio_normal>0.9);
 diff_lethal_normal = normal.genes(grRatio_cancer>0.9 & grRatio_normal<0.1);
 
 % diff_lethal_cancer, aka those lethal in cancer but not in normal
+% diff_lethal_normal, aka those lethal in normal but not in cancer
 
-for i = 1:length(diff_lethal_cancer)
-    candidate_gene = diff_lethal_cancer(i,1);
-    
+type = diff_lethal_cancer
+type = diff_lethal_normal
+
+for i = 1:length(type)
+    candidate_gene = type(i,1)
+
 %     find affected biomass components
     [temp, hasEffect, constrRxnNames, deletedGenes] = deleteModelGenes(cancer, candidate_gene);
     bio_comp = biomassPrecursorCheck(temp);
     
-    file_cell = strcat('lethal_biocomp_', candidate_gene, '.txt')
-    fopen(file_cell{1}, 'w');
-    
-    bio_file = fopen(strcat('lethal_biocomp_', candidate_gene, '.txt'), 'w');
-    fprintf(bio_file, '%s \n', bio_comp{:, 1});
-    fclose(bio_file);
+    file_cell = strcat('lethal_biocomp_', candidate_gene, '.txt');
+%     fopen(file_cell{1}, 'w');
+%     fprintf(file_cell{1}, '%s \n', bio_comp{:, 1});
+%     fclose('all');
+    writetable(table(bio_comp), file_cell{1})
     
 %     find alternative path in ductal epithelium
     [temp, hasEffect, constrRxnNames, deletedGenes] = deleteModelGenes(normal, candidate_gene);
@@ -37,19 +40,9 @@ for i = 1:length(diff_lethal_cancer)
     grRatio_deleted = singleGeneDeletion(temp);
     alt_genes = intersect(normal.genes(grRatio_normal>0.9),temp.genes(grRatio_deleted<0.1));
     
-    alt_cell = strcat('lethal_altgene_', candidate_gene, '.txt')
-    alt_file = fopen(alt_cell{1}, 'w');
-    fprintf(alt_file, '%s \n', alt_genes{:, 1});
-    fclose(alt_file);
+    alt_cell = strcat('lethal_altgene_', candidate_gene, '.txt');
+%     fopen(alt_cell{1}, 'w');
+%     fprintf(alt_cell{1}, '%s \n', alt_genes{:, 1});
+%     fclose('all');
+    writetable(table(alt_genes), alt_cell{1})
 end
-
-
-
-% diff_lethal_normal, aka those lethal in normal but not in cancer
-
-for i = 1:length(diff_lethal_normal)
-    candidate_gene = diff_lethal_normal(i,1);
-    
-    
-end
-
